b16ff0effbf8fbe1e442c491c801710949fe55bd
lijizhi <18310387151@163.com>
2018-12-01 23:36:03 +0800

    增加网关路由并配置重试机制

---
+++ zuul/src/main/java/com/example/zuul/filter/TokenFilter.java
@@ -0,0 +1,52 @@
+package com.example.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class TokenFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(TokenFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("--->>> TokenFilter {},{}", request.getMethod(), request.getRequestURL().toString()));
+        String token  = request.getParameter("token");
+        if (StringUtils.isNotBlank(token)) {
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+//            ctx.getResponse().getWriter().write("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+
+    }
+}
