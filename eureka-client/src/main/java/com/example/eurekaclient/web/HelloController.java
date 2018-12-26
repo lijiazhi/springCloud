453f3370170eb4285ffb881ccc8f7a016391df99
lijiazhi <18310387151@163.com>
2018-12-26 17:47:27 +0800

    引入rabbitMQ,实现配置中心自动刷新

--- eureka-client/src/main/java/com/example/eurekaclient/web/HelloController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/web/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.eurekaclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${server.port}")
     private String port;
@@ -21,5 +23,6 @@ public class HelloController {
     public String hi(){
         return datUrl;
     }
+
 
 }
